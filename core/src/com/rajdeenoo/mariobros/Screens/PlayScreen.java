69939dd9ff0e09f43f14cf4e97423fc76b9064b2
raj deenoo <raj.deenoo@gmail.com>
2017-05-01 12:06:45 +0200

    Part 3 : ViewPort

--- core/src/com/rajdeenoo/mariobros/Screens/PlayScreen.java
+++ core/src/com/rajdeenoo/mariobros/Screens/PlayScreen.java
@@ -3,7 +3,12 @@ package com.rajdeenoo.mariobros.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.rajdeenoo.mariobros.MarioBros;
 
 /**
@@ -14,10 +19,15 @@ public class PlayScreen implements Screen{
 
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(800,480,gamecam);
+
     }
 
 
@@ -30,6 +40,9 @@ public class PlayScreen implements Screen{
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(gamecam.combined);
+
         game.batch.begin();
         game.batch.draw(texture,0, 0);
         game.batch.end();
@@ -37,7 +50,7 @@ public class PlayScreen implements Screen{
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
