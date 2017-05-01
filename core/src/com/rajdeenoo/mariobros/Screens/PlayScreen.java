d9f45f14c360913a747ac52b1c3e90e52268844a
raj deenoo <raj.deenoo@gmail.com>
2017-05-01 12:02:22 +0200

    Part 2 : PlayScreen

---
+++ core/src/com/rajdeenoo/mariobros/Screens/PlayScreen.java
@@ -0,0 +1,62 @@
+package com.rajdeenoo.mariobros.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.rajdeenoo.mariobros.MarioBros;
+
+/**
+ * Created by raj on 01/05/2017.
+ */
+
+public class PlayScreen implements Screen{
+
+    private MarioBros game;
+    Texture texture;
+
+    public PlayScreen(MarioBros game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture,0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
